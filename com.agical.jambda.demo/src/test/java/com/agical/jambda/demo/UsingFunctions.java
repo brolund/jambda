7ee3bbb7cec7115398f075d9d79062eb33dc34f6
daniel <devnull@localhost>
2008-12-19 10:51:51 +0100

    Changed to Bumblebee1.0.3. Added formatting file for eclipse. Added first example

--- com.agical.jambda.demo/src/test/java/com/agical/jambda/demo/UsingFunctions.java
+++ com.agical.jambda.demo/src/test/java/com/agical/jambda/demo/UsingFunctions.java
@@ -1,18 +1,37 @@
 package com.agical.jambda.demo;
 
+import java.util.Date;
+
 import org.junit.Test;
 
+import com.agical.bumblebee.junit4.Storage;
+import com.agical.jambda.Functions.Fn0;
 
 public class UsingFunctions {
-	/*!!
-	Functions are like mappings from zero or more types to a single output type. 
-	*/
-	
-	@Test
-	public void someTest() throws Exception {
-		/*!
-		The simpest function is =Fn0=, which maps to a specific type from nothing.
-		*/
-	}
-	
+    /*!!
+    Functions are like mappings from zero or more types to a single output type. 
+    */
+
+    @Test
+    public void theSimplestFunction() throws Exception {
+        /*!
+        The simpest function is =Fn0=, which maps to a specific type from nothing.
+        */
+        Fn0<String> helloWorldFunction = new Fn0<String>() {
+            @Override
+            public String apply() {
+                return "Hello World at " + new Date();
+            }
+        };
+        String greeting = helloWorldFunction.apply();
+        /*!
+        The =greeting= variable is:
+        >>>>
+        #{greeting}
+        <<<<
+        */
+        
+        Storage.store("greeting", greeting);
+    }
+    
 }
