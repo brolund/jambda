d2531226c042af83c171fbd19e6dc1c334a9c3a8
daniel <devnull@localhost>
2008-12-25 10:58:14 +0100

    Smaller updates. New class for documenting Cons

---
+++ com.agical.jambda.demo/src/test/java/com/agical/jambda/demo/UsingCons.java
@@ -0,0 +1,16 @@
+package com.agical.jambda.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsingCons {
+    /*!
+     * 
+     */
+    @Test
+    public void testname() throws Exception {
+        
+    }
+    
+}
